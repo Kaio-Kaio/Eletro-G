6448072f89963f698ed75fb317adc506ac7b20c9
Kaio-Kaio <kaioeduardoalmeidaalves@gmail.com>
2024-09-24 11:12:04 -0300

    Add files via upload

---
+++ Untitled-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    char nome[4]={"Kaio"};
+    printf("%c", nome[0]);
+    printf("%c", nome[1]);
+    printf("%c", nome[2]);
+    printf("%c", nome[3]);
+   
+}
